1d625d80c7865f7cfb91eb4dd65b12c5abb6a0c3
bokuweb <bokuweb12@gmail.com>
2016-01-20 20:53:39 +0900

    Add overlay

--- index.android.js
+++ index.android.js
@@ -11,66 +11,65 @@ import React, {
 
 const { height: deviceHeight, width: deviceWidth} = Dimensions.get('window');
 
-class Modal extends Component {
+class Example extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      modal: false
-    };
+    this.state = { isOpen: false };
   }
 
-  open () {
-    this.setState({modal: true});
+  openModal () {
+    this.setState({ isOpen: true });
+  }
+
+  closeModal () {
+    this.setState({ isOpen: false });
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <App openModal={this.open.bind(this)}/>
-        {this.state.modal ? <TopModal closeModal={() => this.setState({modal: false}) }/> : null }
+        <View style={styles.flexCenter}>
+          <TouchableOpacity onPress={this.openModal.bind(this)}>
+            <Text>Open Modal</Text>
+          </TouchableOpacity>
+        </View>
+        <Modal isOpen={this.state.isOpen}>
+          <TouchableOpacity onPress={this.closeModal.bind(this)}>
+            <Text>Close Modal</Text>
+          </TouchableOpacity>
+        </Modal>
       </View>
     );
   }
 }
 
-class App extends Component {
-  render() {
-    return (
-      <View style={styles.flexCenter}>
-        <TouchableOpacity onPress={this.props.openModal}>
-          <Text>Open Modal</Text>  
-        </TouchableOpacity>
-      </View>
-    );
-  }
-}
-
-class TopModal extends Component {
+class Modal extends Component {
   constructor(props) {
     super(props);
     this.state = { offset: new Animated.Value(deviceHeight) };
   }
 
-  componentDidMount() {
-    Animated.timing(this.state.offset, {
-      duration: 100,
-      toValue: 0
-    }).start();
-  }
-
-  closeModal() {
-    Animated.timing(this.state.offset, {
-      duration: 100,
-      toValue: deviceHeight
-    }).start(this.props.closeModal);
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.isOpen) {
+      Animated.timing(this.state.offset, {
+        duration: 100,
+        toValue: 0
+      }).start();
+    } else {
+      //Animated.timing(this.state.offset, {
+      //  duration: 100,
+      //  toValue: deviceHeight
+      //}).start(this.props.closeModal);
+    }
   }
 
   render() {
+    if (!this.props.isOpen) return null;
     return (
-      <Animated.View style={[styles.modal, styles.flexCenter, {transform: [{translateY: this.state.offset}]}]}>
-        <TouchableOpacity onPress={this.closeModal.bind(this)}>
-          <Text style={{color: '#FFF'}}>Close Menu</Text>
-        </TouchableOpacity>
+      <Animated.View style={[styles.overlay, styles.flexCenter, {transform: [{translateY: this.state.offset}]}]}>
+        <View style={styles.modal}>
+          {this.props.children}
+        </View>
       </Animated.View>
     );
   }
@@ -85,14 +84,22 @@ const styles = StyleSheet.create({
     justifyContent: 'center', 
     alignItems: 'center'
   },
+  overlay: {
+    backgroundColor: 'rgba(0,0,0,0.8)',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: deviceWidth,
+    height: deviceHeight
+  },
   modal: {
     backgroundColor: '#FF0000',
     position: 'absolute',
-    top: (deviceHeight - 280) / 2,
+    top: (deviceHeight - 300) / 2,
     left: (deviceWidth - 280) / 2,
     width:280,
     height:300
   }
 });
 
-AppRegistry.registerComponent('reactNativeModalAndroid', () => Modal);
+AppRegistry.registerComponent('reactNativeModalAndroid', () => Example);
